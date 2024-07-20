ed9cd77b5cf7860678a372a25f1fce42b7a426dc
nachoneca <8666@holbertonstudents.com>
2024-07-20 14:17:13 -0300

    Pushing excercises 0 to 12

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,8 @@
+export default function taskBlock(trueOrFalse) {
+    /* eslint-disable-next-line no-unused-vars */
+      const unused = trueOrFalse;
+      const task = false;
+      const task2 = true;
+    
+      return [task, task2];
+    }
