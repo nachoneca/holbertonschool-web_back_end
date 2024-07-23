3033130ddf9c3d6e1e6586242524655423a67bd8
nachoneca <8666@holbertonstudents.com>
2024-07-22 21:26:48 -0300

    fix

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -12,7 +12,7 @@ export default class HolbertonCourse {
   set name(name) {
     if (typeof name === 'string') {
       this._name = name;
-    } 
+  }
     else {
       throw new TypeError('Name must be a string');
     }
