71977ae58d524341acb8df80447de685775d306d
nachoneca <8666@holbertonstudents.com>
2024-07-23 18:40:46 -0300

    more stf

--- ES6_classes/eslint.config.mjs
+++ ES6_classes/eslint.config.mjs
@@ -1,6 +1,14 @@
-import config from "eslint-config-standard";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
 
 
 export default [
-  ...[].concat(config),
+  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
 ];
