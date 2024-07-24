11680eb133f4b1704f0685d29b6a539e098f76a6
nachoneca <8666@holbertonstudents.com>
2024-07-24 10:39:14 -0300

    change sides

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,14 @@
+export default function getListStudents() {
+    function Object(id, firstName, location) {
+      this.id = id;
+      this.firstName = firstName;
+      this.location = location;
+    }
+    const students = [];
+    const Guillaume = new Object(1, 'Guillaume', 'San Francisco');
+    const Jamese = new Object(2, 'James', 'Columbia');
+    const Serena = new Object(5, 'Serena', 'San Francisco');
+    students.push(Guillaume, Jamese, Serena);
+  
+    return students;
+}
