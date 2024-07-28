4f8a39182f20194087c27c6e76cc608d5849f65a
nachoneca <8666@holbertonstudents.com>
2024-07-28 13:25:19 -0300

    Ex 0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const True = ('');
+    if (True) {
+      resolve;
+    } else {
+      reject;
+    }
+  });
+}
