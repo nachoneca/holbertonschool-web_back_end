ef6faaaee8a9f69a59740d7d9143545a6eeefaf4
nachoneca <8666@holbertonstudents.com>
2024-07-28 14:45:08 -0300

    fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
+  return Promise.all([uploadPhoto(), createUser()]).then(
+    (values) => {
       const [photo, user] = values;
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     },
     () => {
       console.log('Signup system offline');
-    });
+    }
+  );
 }
