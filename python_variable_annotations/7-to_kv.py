61536f3e163926f479a37b5e4be10cfb093dc7bc
nachoneca <8666@holbertonstudents.com>
2024-08-14 00:07:28 -0300

    Ex 7 Takes str and float and return tuple of str and float^2

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
 """Takes str and float and return tuple of str and float^2"""
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Takes str and float and return tuple of str and float^2"""
     return (k, v ** 2)
-
