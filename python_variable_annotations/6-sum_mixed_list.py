b2c6e383db24f671b31b7f26044e000ce44ce258
nachoneca <8666@holbertonstudents.com>
2024-08-13 23:47:39 -0300

    Ex 6 Sum list of floats and ints

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from typing import List, Union
+
+"""Sum list of floats and ints"""
+
+
+def sum_list(input_list: List[Union[float, int]]) -> float:
+    """Sum list of floats and ints"""
+    return float(sum(input_list))
