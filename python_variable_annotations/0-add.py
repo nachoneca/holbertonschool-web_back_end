52249e057beea6b5e351e98ae3919f88e93a818c
nachoneca <8666@holbertonstudents.com>
2024-08-13 23:18:28 -0300

    Ex 0 add-float

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-
 """function for sum two floats"""
 
 
-
-def add(a: float, b: float):
+def add(a: float, b: float) -> float:
     """function for sum two floats"""
     return a + b
