9c00b6b0e6071b38f1177a09659acdee9fa4f34d
nachoneca <8666@holbertonstudents.com>
2024-08-15 22:10:16 -0300

    Ex 1 wait_random n times and save delay into a list

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""wait_random n times and save delay into a list"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_random n times and save delay into a list"""
+    del_list = []
+    for x in range(n):
+        diley = await wait_random(max_delay)
+        del_list.append(diley)
+    return del_list
