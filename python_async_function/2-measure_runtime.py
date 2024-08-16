ce6ce65265d1de574074327c483c9c5ee8d04349
nachoneca <8666@holbertonstudents.com>
2024-08-16 14:41:40 -0300

    Ex 2 takes n coroutines and max delay is the time it will sleep     return time of each coroutine

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""takes n coroutines and max delay is the time it will sleep
+return time of each coroutine"""
+
+import time
+import asyncio
+import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """takes n coroutines and max delay is the time it will sleep
+    return time of each coroutine"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
