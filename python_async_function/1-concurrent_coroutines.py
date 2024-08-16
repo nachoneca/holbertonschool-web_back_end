6d12a6678fb0984ef94a1a31d13e9451c03d3604
nachoneca <8666@holbertonstudents.com>
2024-08-16 00:06:13 -0300

    fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-   """Saves corutines in tasks, 
+    """Saves corutines in tasks, 
     and waits every result as finished and save
     it in a list called delays"""
     asyncr = [wait_random(max_delay) for _ in range(n)]
