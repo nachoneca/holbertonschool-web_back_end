d167e63a36eb0046425564f00f897ea67e3f21e7
nachoneca <8666@holbertonstudents.com>
2024-08-16 17:06:45 -0300

    Ex 4 Creates a list of corutines(tasks) and save them when they are completed into a list in that way it will be sorted atomatically

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""Execute n times the wait_random
-    function with the max_delay for arg
-    and return a list of those random numbers"""
+"""Creates a list of corutines(tasks) and save them
+when they are completed into a list in 
+that way it will be sorted atomatically"""
 
 
 import asyncio
 import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
-task_wait_random = __import__('3-task').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Execute n times the wait_random
-    function with the max_delay for arg
-    and return a list of those random numbers"""
-    tasks = [asyncio.run(task_wait_random(max_delay))for _ in range(n)]
+    """Creates a list of corutines(tasks) and save them
+    when they are completed into a list in 
+    that way it will be sorted atomatically"""
+    tasks = [task_wait_random(max_delay)for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
