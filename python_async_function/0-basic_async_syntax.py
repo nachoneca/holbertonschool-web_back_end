c31989eb5d7618d4b54e8c70ce10bc65c6f5f63e
nachoneca <8666@holbertonstudents.com>
2024-08-15 21:55:53 -0300

    Ex 0 Takes a num, awayst for num random and return delay

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Takes a num, awayst for num random and return delay"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Takes a num, awayst for num random and return delay"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
