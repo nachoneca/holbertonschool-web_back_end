77d12721ff066c12b722c386b2670b2e976accd0
nachoneca <8666@holbertonstudents.com>
2024-08-15 21:25:02 -0300

    Ex 9 Takes an iterable lst and return a list of secuence and its lenth

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,5 +5,5 @@ from typing import List, Iterable, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Takes an iterable lst and return a list of each secuence and its lenth"""
+    """Takes an iterable lst and return a list of secuence and its lenth"""
     return [(i, len(i)) for i in lst]
