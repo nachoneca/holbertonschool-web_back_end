7a031d8e29423c15a53362b0f978bbed7f0fd0ef
nachoneca <8666@holbertonstudents.com>
2024-08-15 23:21:18 -0300

    FIX?

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,6 +12,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """Saves corutines in tasks, 
     and waits every result as finished and save
     it in a list called delays"""
+    
     tasks = [wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
     
