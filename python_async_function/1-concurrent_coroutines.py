2fff09a60c66adbf48dd4a991d984f4ee6901554
nachoneca <8666@holbertonstudents.com>
2024-08-16 00:14:24 -0300

    fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
-"""Saves corutines in tasks, 
-    and waits every result as finished and save
-    it in a list called delays"""
-import asyncio
+"""wait_random n times and save delay into a list"""
 from typing import List
-import heapq
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Saves coroutines in tasks,
-    and waits every result as finished and saves
-    it in a list called delays in ascending order"""
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = []
-    
-    # Use a heap to maintain order while inserting results
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        heapq.heappush(delays, delay)  # Maintain the list in ascending order
-    
-    return delays
+    """wait_random n times and save delay into a list"""
+    del_list = []
+    for x in range(n):
+        diley = await wait_random(max_delay)
+        del_list.append(diley)
+    return del_list
