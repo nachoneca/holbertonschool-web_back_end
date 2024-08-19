e91afdfed6c06806e259c4885008297d659c0505
nachoneca <8666@holbertonstudents.com>
2024-08-19 13:07:59 -0300

    Fix?

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,9 +8,9 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Function that loops 10 time and sleep 1 sec asyncronusly,
 then yield one number"""
     for loop in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.uniform(0.0, 10.0)
