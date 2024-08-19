453e72e44529a26b3786b37963935f53542dc3d6
nachoneca <8666@holbertonstudents.com>
2024-08-19 13:24:58 -0300

    Ex 1 Async func that iterates inside async_generator and saves the float into a list

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
+"""Async func that iterates inside async_generator
+and saves the float into a list"""
+
 
 import asyncio
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Async func that iterates inside async_generator
+and saves the float into a list"""
+    result = []
+    async for x in async_generator():
+        result.append(x)
+    return result
