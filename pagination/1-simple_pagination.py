7f0294e2320486fc66cc6b6d2594997541665e8a
nachoneca <8666@holbertonstudents.com>
2024-08-20 14:12:05 -0300

    Ex 1 Func that get page and page size, uses the func         index_range to get all the data inside that range

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Func that takes the page and page size, to return the
+intex of the starting element and the ending element"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Func that get page and page size, uses the func
+        index_range to get all the data inside that range"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+        if len(data) < end_index:
+            return []
+        total_data = data[start_index:end_index]
+        return total_data
